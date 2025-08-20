fc24cc7dc3b345d018d732652e955690381fa664
Sri Venkata Krishna Sourav V <krishnavitap@gmail.com>
2025-08-21 03:08:07 +0530

    Update settings.py

--- EM/settings.py
+++ EM/settings.py
@@ -5,6 +5,7 @@ This is the latest update
 
 import os
 from pathlib import Path
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
